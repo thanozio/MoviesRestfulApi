6ce0189202953414c381a4ce59d52090785853df
Thanozio <thanozio@proton.me>
2025-03-17 12:09:46 +0200

    chore: update .gitignore

--- MovieRatings.Application/Models/Movie.cs
+++ MovieRatings.Application/Models/Movie.cs
@@ -7,18 +7,22 @@ public partial class Movie
     public required Guid Id { get; init; }
 
     public required string Title { get; set; }
-    public string Slug => CreateSlug();
+
+    public string Slug => GenerateSlug();
 
     public required int YearOfRelease { get; set; }
 
     public required List<string> Genres { get; init; } = new();
 
-    private string CreateSlug()
+    private string GenerateSlug()
     {
-        var sluggedTitle = SlugRegex().Replace(Title, string.Empty);
-        return $"{sluggedTitle.ToLower().Replace(" ", "-")}-{YearOfRelease}";
+        var sluggedTitle = SlugRegex()
+            .Replace(Title, string.Empty)
+            .ToLower()
+            .Replace(" ", "-");
+        return $"{sluggedTitle}-{YearOfRelease}";
     }
 
-    [GeneratedRegex("[^a-zA-Z0-9 _-]", RegexOptions.NonBacktracking, 5)]
+    [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
     private static partial Regex SlugRegex();
 }
